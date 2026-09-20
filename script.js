d3beaede921051cb1f537b02b932e022ad06c676
EstivenPapu <bboypapufull7@gmail.com>
2026-09-20 18:33:26 -0500

    terminasion de MVP para reunion JAC

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const formulario = document.getElementById('formulario');
 const numeroCasa = document.getElementById('casa');
 const contenedor = document.getElementById('resultado');
 
+
+
 const vecinos = [
     {
         casa: 15,
@@ -51,59 +53,72 @@ const vecinos = [
 formulario.addEventListener('submit', function(event) {
     event.preventDefault();
 
+    let htmlMultas = "";
+    let totalMultas = 0;
+    let contador = 0;
+
     const numero = Number(numeroCasa.value.trim()) 
 
     if (numero === 0) {
 
         alert('Por favor, ingrese su número de casa.');
         return;
-
+        
     }else {
 
         const vecinoEncontrado = vecinos.find(vecino => vecino.casa === numero);
+
         console.log(vecinoEncontrado);
         // Mostrar el resultado de la consulta
         document.getElementById('resultado').classList.remove('oculto');
+        if (vecinoEncontrado !== undefined){
+            vecinoEncontrado.multas.forEach(multa => {
+                htmlMultas += `
+                    <div class="tarjeta-multa">
 
-        
-
-        let htmlMultas = "";
-        let totalMultas = 0;
-        let contador = 0;
-        vecinoEncontrado.multas.forEach(multa => {
-            htmlMultas += `
-                <div class="tarjeta-multa">
-
-                    <div class="info">
-                        <h5>Motivo: ${multa.motivo}</h5>
-                        <h5>${multa.fecha}</h5>
+                        <div class="info">
+                            <h5>Motivo: ${multa.motivo}</h5>
+                            <h5>${multa.fecha}</h5>
+                        </div>
+                        <div class="values">
+                            <h5 class="estado">${multa.estado}</h5>
+                            <h5 class="valor">$${multa.valor}</h5>
+                        </div>
+            
                     </div>
-                    <div class="values">
-                        <h5 class="estado">${multa.estado}</h5>
-                        <h5 class="valor">$${multa.valor}</h5>
+                `
+                contador++;
+                totalMultas += Number(multa.valor);
+            })
+
+            let htmlGenerado = `
+                <h1>Resultado de la consulta</h1>
+                <div class="tarjeta-vecino">
+                    <h4>Numero casa: ${vecinoEncontrado.casa}</h4>
+                    <h4>Propietario: ${vecinoEncontrado.nombre}</h4>
+                    
+                    <div class="titulo-multas">
+                        <h2>Multas registradas</h2>
+                        <h4>Multas pendientes (${contador})</h4>
                     </div>
-    
+
+                    <hr>
+                    
+                    <div class="multas">
+                        ${htmlMultas || `<p>Este vecino no tiene multas registradas</p>`}
+                    </div>
+                    <div class="total">
+                        <h4>Total multas</h4> <h3>$${totalMultas}</h3>
+                    </div>
+
                 </div>
-            `
-            contador++;
-            totalMultas += Number(multa.valor);
-        })
+            `;
 
-        let htmlGenerado = `
-            <h2>Resultado de la consulta</h2>
-            <div class="tarjeta-vecino">
-                <h4>Numero casa: ${vecinoEncontrado.casa}</h4>
-                <h4>Propietario: ${vecinoEncontrado.nombre}</h4>
-                <h2>Multas registradas</h2>
-                <h6>Multas pendientes (${contador})</h6>
-                <div class="multas">
-                    ${htmlMultas || `<p>Este vecino no tiene multas registradas</p>`}
-                </div>
-                <h4>Total multas: $${totalMultas}</h4>
-            </div>
-        `;
-
-        contenedor.innerHTML = htmlGenerado;
-
+            contenedor.innerHTML = htmlGenerado;
+        }else{
+            contenedor.innerHTML = `
+            <p class="error">Dijite un numero de casa existente</p>
+            `;
+        }
     }
 });   
